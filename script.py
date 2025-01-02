af9904888545d931abd8e239e740786bb895bf23
MaxKolleCAT <49076129+MaxKolleCAT@users.noreply.github.com>
2025-01-02 12:00:13 +0000

    Übung abgeschlossen.

--- script.py
+++ script.py
@@ -15,3 +15,39 @@
 # Wörter: 5
 
 # Optional: Erweitern Sie das Skript, um auch die Anzahl der Vokale und Konsonanten zu zählen.
+
+import argparse
+
+descrpiton_str = "Das Skript untersucht den eigegeben String und gibt die Anzahl der Zeichen und weitere Details aus."
+
+# Argumente verarbeiten
+parser = argparse.ArgumentParser(description=descrpiton_str)
+parser.add_argument("input_str", type=str, help="String der analysiert werden soll")
+parser.add_argument("--details", action='store_true', help="Ausgabe von mehr Details zum eingegeben String")
+args = parser.parse_args()
+
+num_chars = len(args.input_str)
+
+print("Ausgabe:")
+print("Anzahl der Zeichen: ", num_chars)
+
+if args.details==True:
+    # Leerzeichen zählen -> Wörter
+    num_words = str.count(args.input_str, " ")+1
+    print("Anzahl Wörter: ", num_words)
+    
+    # Anzahl der Konsuanten bestimmen
+    key_str = "aeiou"
+    count_int = 0
+    for char in args.input_str:
+        if char.lower() in key_str:
+            count_int = count_int+1
+    print("Anzahl Konsunaten: ", count_int)
+
+    # Anzahl der Vokale bestimmen
+    key_str = "bcdfghjklmnpqrstvwxyz"
+    count_int = 0
+    for char in args.input_str:
+        if char.lower() in key_str:
+            count_int = count_int+1
+    print("Anzahl Vokale: ", count_int)
