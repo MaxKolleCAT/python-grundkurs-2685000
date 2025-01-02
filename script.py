ee66c385b96ea7721f47a86006ac8c1d2d2f7e84
MaxKolleCAT <49076129+MaxKolleCAT@users.noreply.github.com>
2025-01-02 10:19:50 +0000

    Übung abgeschlossen.

--- script.py
+++ script.py
@@ -18,3 +18,39 @@
 # 4. Das Programm führt die Berechnung durch und gibt das Ergebnis aus.
 
 # Optional: Erweitern Sie den Taschenrechner um weitere Funktionen wie Potenzierung oder Modulo.
+
+print("Python Taschenrechner")
+print("(C) Max Kolletzki, 2025")
+
+while 1:
+  op = input("Welche Operation (+, -, *, /) möchtest du durchführen? ")
+  
+  # Eingabe der ersten Zahl
+  zahl1 = input("Bitte gib die 1. Zahl ein: ")
+  try:
+    zahl1 = float(zahl1)
+  except:
+    print("Problem bei der Konvertierung der 1. Zahl. Bitte versuche es erneut\n")
+    continue
+  
+  # Eingabe der zweiten Zahl
+  zahl2 = input("Bitte gib die 2. Zahl ein: ")
+  try:
+    zahl1 = float(zahl2)
+  except:
+    print("Problem bei der Konvertierung der 2. Zahl. Bitte versuche es erneut\n")
+    continue
+
+  # Durchführung der Berechnung
+  if op == '+':
+    print("Ergebnis der Addition: ", (zahl1+zahl2))
+  elif op == '-':
+    print("Ergebnis der Subtraktion: ", (zahl1-zahl2))
+  elif op == '*':
+    print("Ergebnis der Multiplikation: ", (zahl1*zahl2))
+  elif op == '/':
+    print("Ergebnis der Division: ", (zahl1/zahl2))
+  else:
+    print("Operation unbekannt, bitte versuche es noch einmal.")
+
+  print("")
