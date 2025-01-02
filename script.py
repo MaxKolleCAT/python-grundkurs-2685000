b14090fb452e5e2e59f2a484c420eb94cff4ac30
MaxKolleCAT <49076129+MaxKolleCAT@users.noreply.github.com>
2025-01-02 11:07:32 +0000

    Übung abgeschlossen

--- script.py
+++ script.py
@@ -16,3 +16,32 @@ import argparse
 #    - Bei der Division soll eine Fehlerbehandlung implementiert werden, um eine Division durch Null zu vermeiden.
 
 # 4. Geben Sie das Ergebnis der Berechnung aus.
+
+print("Python Taschenrechner mit argparse")
+print("(C) Max Kolletzki, 2025")
+
+# Parsen der CLI-Argumente
+parser = argparse.ArgumentParser(description="Argumente für Taschenrechner")
+parser.add_argument("zahl1", help="Erste Zahl für Taschenrechner", type=int)
+parser.add_argument("zahl2", help="Zweite Zahl für Taschenrechner", type=int)
+parser.add_argument("--operation", help="Operation für den Taschenrechner", choices=["add", "sub", "mul", "div"], default="add")
+args = parser.parse_args()
+
+# Default argument für Operation
+#if args.operation == None:
+#    args.operation = "add"
+
+# Durchführung der Berechnung
+if args.operation == 'add':
+    print("Ergebnis der Addition: ", (args.zahl1+args.zahl2))
+elif args.operation == 'sub':
+    print("Ergebnis der Subtraktion: ", (args.zahl1-args.zahl2))
+elif args.operation == 'mul':
+    print("Ergebnis der Multiplikation: ", (args.zahl1*args.zahl2))
+elif args.operation == 'div':
+    if args.zahl2!=0:
+        print("Ergebnis der Division: ", (args.zahl1/args.zahl2))
+    else:
+        print("Fehler: Division durch 0.")
+
+print("")
